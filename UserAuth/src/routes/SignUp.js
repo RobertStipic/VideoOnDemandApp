579495aebe96c3dc8c1631e447b84730b66025a8
Robert Stipic <stile351@gmail.com>
2025-01-08 02:04:25 +0100

    adding user validation

--- UserAuth/src/routes/SignUp.js
+++ UserAuth/src/routes/SignUp.js
@@ -1,10 +1,54 @@
 import express from 'express';
-
+import {body , validationResult} from 'express-validator';
 
 const SingUpRouter = express.Router();
 
-SingUpRouter.post('/users/singup', (req, res) =>{
+SingUpRouter.post('/users/signup',[
+    body('email')
+    .isEmail()
+    .withMessage("Email is not valid"),
+    body('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please enter password')
+    .isString()
+    .withMessage('Password should be string')
+    .isLength({min: 8, max: 20})
+    .withMessage('Password must be between 8 and 20 characters'),
+    body('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage("Please enter firstname")
+    .isAlpha('sr-RS@latin')
+    .withMessage('first name is not valid(only characters)'),
+    body('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage("Please enter lastname")
+    .isAlpha('sr-RS@latin')
+    .withMessage('first name is not valid(only characters)'),
+    body('dateOfBirth')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide Date of Birth")
+    .isDate()
+    .withMessage("Date format must be: YYYY-MM-DD"),
+    body('country')
+    .isISO31661Alpha2()
+    .withMessage('Please provide valid 2 letter ISO 3166-1 Country code'),
+    body('city')
+    .exists({ checkFalsy: true })
+    .isAlpha('sr-RS@latin')
+    .withMessage('City name is not valid(only characters)'),
+    body('gender')
+    .exists({checkFalsy: true})
+    .withMessage('Please provide gender')
+    .isIn(["Male", "Female", "Other"])
+    .withMessage("Valid gender values: Male,Female,Other")
+], (req, res) =>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+            return res.status(400).send(errors.array());
+    }
+ const {email, password, firstName, lastName, dateOfBirth, country, city, gender } = req.body;
 
+ res.status(200).send('user created');
 });
 
 export {SingUpRouter};
