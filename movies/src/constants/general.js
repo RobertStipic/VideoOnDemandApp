7c26c06d673f09fc59d34c286b74769ecd1e64ab
Robert Robi <stile351@gmail.com>
2025-03-07 16:09:06 +0100

    exported constants other file, implemented logic to find movies by year, or range of years and sort them

---
+++ movies/src/constants/general.js
@@ -0,0 +1,13 @@
+export const requestYears = {
+  year: "year",
+  startyear: "startyear",
+  endyear: "endyear",
+  minValue: 1931,
+  maxValue: 2021,
+  sorting: "sorting",
+  sort: {
+    ascending: "ascending",
+    descending: "descending",
+    values: ["ascending", "descending"],
+  },
+};
--- movies/src/routes/MoviesByYear.js
+++ movies/src/routes/MoviesByYear.js
@@ -1,44 +1,70 @@
-import express from 'express';
-import { body, validationResult } from 'express-validator';
-import { currentUser, userAuthorization } from '@robstipic/middlewares';
-import { Movie } from '../models/movies.js';
-
+import express from "express";
+import { body, validationResult } from "express-validator";
+import { currentUser, userAuthorization } from "@robstipic/middlewares";
+import { requestYears } from "../constants/general.js";
+import { Movie } from "../models/movies.js";
 
 const MoviesByYearRouter = express.Router();
 
-MoviesByYearRouter.get('/movies/moviesbyyear', [
-body('year')
-.isInt({min: 1931, max:2021})
-.withMessage('Valid start year values: 1931-2021'),
-body('startyear')
-.isInt({min: 1931, max:2021})
-.withMessage('Valid start year values: 1931-2021'),
-body('endyear')
-.isInt({min: 1931, max:2021})
-.withMessage('Valid start year values: 1931-2021')
-]
-,currentUser, 
-userAuthorization, 
-async (req, res) =>{
+MoviesByYearRouter.get(
+  "/movies/moviesbyyear",
+  [
+    body(requestYears.year)
+      .optional()
+      .isInt({ min: requestYears.minValue, max: requestYears.maxValue })
+      .withMessage(
+        `Valid ${requestYears.year} values: ${requestYears.minValue}-${requestYears.maxValue}`
+      ),
+    body(requestYears.startyear)
+      .optional()
+      .isInt({ min: requestYears.minValue, max: requestYears.maxValue })
+      .withMessage(
+        `Valid ${requestYears.startyear} values: ${requestYears.minValue}-${requestYears.maxValue}`
+      ),
+    body(requestYears.endyear)
+      .optional()
+      .isInt({ min: requestYears.minValue, max: requestYears.maxValue })
+      .withMessage(
+        `Valid ${requestYears.endyear} values: ${requestYears.minValue}-${requestYears.maxValue}1`
+      ),
+    body(requestYears.sorting)
+      .optional()
+      .isIn(requestYears.sort.values)
+      .withMessage(
+        `Valid ${requestYears.sorting} values: ${requestYears.sort.ascending} & ${requestYears.sort.descending}`
+      ),
+  ],
+  currentUser,
+  userAuthorization,
+  async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()){
-            return res.status(400).send(errors.array());
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
     }
-    const {year, startyear, endyear} = req.body;
+    const { year, startyear, endyear, sorting } = req.body;
 
-    if(year && startyear || endyear){
-        res.status(400).send("You can filter only by year, or startyear-endyear(valid values: 1931:2021)")
-    }
-    else if(year && !startyear && !endyear){
-        let movies = await Movie.find({"Year": {$where: year}},{Title:1, Year: 1});
-        res.status(200).send(movies);
-    }
-    else if(year && !startyear && !endyear){
-        let movies = await Movie.find({"Year": {$gte: startyear, $lte: endyear}},{Title:1, Year: 1});
-        res.status(200).send(movies);
-    }
+    if (year && (startyear || endyear)) {
+      res
+        .status(400)
+        .send(
+          `You can filter only by ${requestYears.year}, or ${requestYears.startyear}-${requestYears.endyear}(valid values are from: ${requestYears.minValue} to ${requestYears.maxValue})`
+        );
+    } else if (year) {
+      let movies = await Movie.find(
+        { Year: { $eq: year } },
+        { Title: true, Year: true }
+      );
+      res.status(200).send(movies);
+    } else if (startyear && endyear) {
+      const sortOrder = sorting === requestYears.sort.descending ? -1 : 1;
 
-    let movies = await Movie.find({"Year": {"$regex": language, "$options": "i"}},{Title:1, Language: 1});
-    res.status(200).send(movies);
-});
-export {MoviesByYearRouter};
+      let movies = await Movie.find({
+        Year: { $gte: startyear, $lte: endyear },
+      })
+        .sort({ Year: sortOrder })
+        .select({ Title: true, Year: true });
+      res.status(200).send(movies);
+    }
+  }
+);
+export { MoviesByYearRouter };
