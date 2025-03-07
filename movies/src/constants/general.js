dc186ceb7f3d7342eb252dfc660eff111f543f05
Robert Robi <stile351@gmail.com>
2025-03-07 16:24:34 +0100

    exported constants in constants file

--- movies/src/constants/general.js
+++ movies/src/constants/general.js
@@ -11,3 +11,17 @@ export const requestYears = {
     values: ["ascending", "descending"],
   },
 };
+export const requestLanguages = {
+  language: "language",
+  // prettier-ignore
+  languagesArray: [
+  "English", "Italian", "Mandarin", "Latin", "Spanish", "Sicilian", "Hebrew",
+  "Polish", "German", "Quenya", "Sindarin", "French", "Japanese", "Czech",
+  "Portuguese", "Swahili", "Xhosa", "Zhulu", "Hungarian", "Cantonese",
+  "Vietnamese", "Arabic", "Nepali", "Hindi", "Esperanto", "Russian",
+  "Korean", "Gaelic", "Yiddish", "Turkish", "Amharic", "Danish",
+  ],
+  languagesMessage: `List of valid languages: English, Italian, Mandarin, Latin, Spanish, German, French, Japanese, Russian, Sicilian, 
+Hebrew, Polish, Quenya, Czech, Portuguese, Swahili, Xhosa, Zhulu, Hungarian, Cantonese, Vietnamese, Arabic, Nepali, Hindi,
+Esperanto, Korean, Gaelic, Yiddish, Turkish, Amharic, Danish`,
+};
--- movies/src/routes/MoviesByLanguage.js
+++ movies/src/routes/MoviesByLanguage.js
@@ -2,33 +2,33 @@ import express from "express";
 import { body, validationResult } from "express-validator";
 import { currentUser, userAuthorization } from "@robstipic/middlewares";
 import { Movie } from "../models/movies.js";
+import { requestLanguages } from "../constants/general.js";
 const MoviesByLanguageRouter = express.Router();
+
 //prettier-ignore
-const languangesArray = ["English", "Italian", "Mandarin", "Latin", "Spanish", "Sicilian", " Hebrew",
-    "Polish", "German", "Quenya", "Sindarin","French", "Japanese", "Czech", "Portuguese",
-    "Swahili", "Xhosa", "Zhulu", "Hungarian", "Cantonese", "Vietnamese", "Arabic",
-    "Nepali", "Hindi", "Esperanto", "Russian", "Korean", "Gaelic", "Yiddish", "Turkish",
-    "Amharic", "Danish"]
-//prettier-ignore
-MoviesByLanguageRouter.get('/movies/moviesbylanguage', [
-body('language')
-.exists({checkFalsy: true})
-.withMessage('Please provide language')
-.isIn(languangesArray)
-.withMessage(`List of valid languages: English, Italian, Mandarin, Latin, Spanish, German, French, Japanese, Russian, Sicilian, 
-Hebrew, Polish, Quenya, Czech, Portuguese, Swahili, Xhosa, Zhulu, Hungarian, Cantonese, Vietnamese, Arabic, Nepali, Hindi,
-Esperanto, Korean, Gaelic, Yiddish, Turkish, Amharic, Danish`)
-]
-,currentUser, 
-userAuthorization, 
-async (req, res) =>{
+MoviesByLanguageRouter.get(
+  "/movies/moviesbylanguage",
+  [
+    body(requestLanguages.language)
+      .exists({ checkFalsy: true })
+      .withMessage("Please provide language")
+      .isIn(requestLanguages.languagesArray)
+      .withMessage(requestLanguages.languagesMessage),
+  ],
+  currentUser,
+  userAuthorization,
+  async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()){
-            return res.status(400).send(errors.array());
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
     }
-    const {language} = req.body;
-    let movies = await Movie.find({"Language": {"$regex": language, "$options": "i"}},{Title:1, Language: 1});
+    const { language } = req.body;
+    let movies = await Movie.find(
+      { Language: { $regex: language, $options: "i" } },
+      { Title: 1, Language: 1 }
+    );
     res.status(200).send(movies);
-});
+  }
+);
 
 export { MoviesByLanguageRouter };
