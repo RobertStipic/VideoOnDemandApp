7b37ce7607c919d3025b6b08b6c01c042ff0ec57
Robert Stipic <stile351@gmail.com>
2025-08-24 23:30:03 +0200

    Changed FindAllPayments route object

--- payment/src/routes/findAllPayments.js
+++ payment/src/routes/findAllPayments.js
@@ -10,7 +10,16 @@ findAllRouter.get(
   userAuthorization,
   async (req, res) => {
     try {
-      const payment = await StripePayment.find({});
+      const payment = await StripePayment.find({}).select({
+        "payment_info.amount": 1,
+        "payment_info.amount_captured": 1,
+        "payment_info.currency": 1,
+        "payment_info.paid": 1,
+        "payment_info.description": 1,
+        "receipt_info.receipt_url": 1,
+        userEmail: 1,
+        id: 1, 
+      });
       console.log("Number of payments", payment.length);
 
       res.status(200).send(payment);
