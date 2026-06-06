409e0e303a8f3291a4d5fad9240b9cd9b9b39515
Robert Robi <stile351@gmail.com>
2026-06-06 15:20:33 +0200

    homepage now display user auth status(signed in/not signed in)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,18 @@
 import buildAxios from "../api/init-axios.js";
 
 const HomePage = ({ currentUser }) => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <h1>Home page</h1>
+      {currentUser ? (
+        <h2>
+          You are signed in as: {currentUser.firstName}, {currentUser.email}
+        </h2>
+      ) : (
+        <h2>You are not signed in.</h2>
+      )}
+    </div>
+  );
 };
 export const getServerSideProps = async (context) => {
   if (!context.req.headers.cookie) {
