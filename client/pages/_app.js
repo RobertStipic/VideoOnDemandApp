2368f3fa8f1b43e82d98db5960f03813d59268c0
Robert Stipic <stile351@gmail.com>
2026-06-18 19:20:57 +0200

    _app.js now fetching currentuser

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import buildAxios from "../api/init-axios.js";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
+
+AppComponent.getInitialProps = async context => { 
+
+if (!context.ctx.req.headers.cookie) {
+    return {pageProps: {currentUser: null}};
+  }
+
+  const client = buildAxios(context.ctx);
+  const response = await client.get("/users/currentuser");
+return {pageProps: { currentUser: response.data}};
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -14,13 +14,6 @@ const HomePage = ({ currentUser }) => {
     </div>
   );
 };
-export const getServerSideProps = async (context) => {
-  if (!context.req.headers.cookie) {
-    return { props: { currentUser: null } };
-  }
-  const client = buildAxios(context);
-  const response = await client.get("/users/currentuser");
-  return { props: { currentUser: response.data } };
-};
+
 
 export default HomePage;
