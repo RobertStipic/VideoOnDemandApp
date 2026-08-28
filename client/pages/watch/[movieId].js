09503606164b4b817cadd372cb080fc4b5e18cfd
Robert Stipic <stile351@gmail.com>
2026-08-28 23:31:57 +0200

    added similar movies section below player

--- client/pages/watch/[movieId].js
+++ client/pages/watch/[movieId].js
@@ -1,11 +1,12 @@
 import dynamic from "next/dynamic";
 import buildAxios from "../../api/init-axios.js";
+import MovieCard from "../../components/movieCard.js";
 
 const MoviePlayer = dynamic(() => import("../../components/moviePlayer.js"), {
   ssr: false,
 });
 
-const WatchMoviePage = ({ movie, isSubscribed, error, notAuthorized }) => {
+const WatchMoviePage = ({ movie, isSubscribed, error, notAuthorized, recommendations }) => {
 
   if (error) {
     return (
@@ -25,7 +26,21 @@ const WatchMoviePage = ({ movie, isSubscribed, error, notAuthorized }) => {
   return (
     <div>
       {isSubscribed ? (
-      <MoviePlayer movie={movie} />
+        <>
+      <MoviePlayer key={movie.imdbID} movie={movie} />
+      {recommendations.length > 0 && (
+        <div className="container mt-4">
+          <h3 className="mb-4"> You may also like </h3>
+          <div className="row g-4">
+            {recommendations.map((rec) => (
+              <div className="col-12 col-md-4" key={rec.imdbID}>
+                <MovieCard movie={rec} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      </>
       ) :(
       <div className="container mt-4">
        <div className="alert alert-warning">
@@ -42,11 +57,13 @@ export async function getServerSideProps(context) {
   const { movieId } = context.query;
   const client = buildAxios(context);
   const { data } = await client.get(`/movies/watch/${movieId}`);
-      return { props: { movie: data , isSubscribed: true }};
+  const recommendationsResponse = await client.get(`/recommendations/${movieId}`)
+
+      return { props: { movie: data , isSubscribed: true, recommendations: recommendationsResponse.data || [] }};
   } catch(error){
 
     if(error.response.status==403){
-      return { props: { movie: null, isSubscribed: false }};
+      return { props: { movie: null, isSubscribed: false, recommendations: [] }};
     }
     else if(error.response.status===401){
         return {props: {notAuthorized: true}}
@@ -58,6 +75,7 @@ export async function getServerSideProps(context) {
         return { props: {error: "Unexpected error"} }
     }
   };
+ 
 }
 
 export default WatchMoviePage;
